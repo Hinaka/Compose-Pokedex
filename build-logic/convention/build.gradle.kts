f84587250d7c3b7f1c7bdccb95e3d8c16907e381
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-08-29 20:29:00 +0700

    build: add spotless plugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -9,4 +9,17 @@ java {
   targetCompatibility = JavaVersion.VERSION_1_8
 }
 
+dependencies {
+  compileOnly(libs.android.gradleplugin)
+  compileOnly(libs.kotlin.gradleplugin)
+  compileOnly(libs.spotless.gradleplugin)
+}
 
+gradlePlugin {
+  plugins {
+    register("spotless") {
+      id = "pokedex.spotless"
+      implementationClass = "SpotlessConventionPlugin"
+    }
+  }
+}
