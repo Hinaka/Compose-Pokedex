66684b6b957ecc59926598a22af66829fc91785e
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-08-29 18:30:49 +0700

    build: apply version catalog

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+  `kotlin-dsl`
+}
+
+group = "dev.hinaka.pokedex.buildlogic"
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+
