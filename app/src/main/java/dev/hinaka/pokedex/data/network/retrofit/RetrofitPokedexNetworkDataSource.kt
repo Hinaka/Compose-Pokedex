8f0849aaaeb5fbd7e13faddaa9f3ca31a5453d99
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-09-03 18:07:59 +0700

    apply spotless

--- app/src/main/java/dev/hinaka/pokedex/data/network/retrofit/RetrofitPokedexNetworkDataSource.kt
+++ app/src/main/java/dev/hinaka/pokedex/data/network/retrofit/RetrofitPokedexNetworkDataSource.kt
@@ -1,16 +1,30 @@
+/*
+ * Copyright 2022 Hinaka (Trung Nguyễn Minh Trần)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.hinaka.pokedex.data.network.retrofit
 
-import android.util.Log
 import dev.hinaka.pokedex.data.network.PokedexNetworkDataSource
 import dev.hinaka.pokedex.data.network.model.NetworkPokemon
 import dev.hinaka.pokedex.data.network.retrofit.api.PokemonApi
+import javax.inject.Inject
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
-import javax.inject.Inject
 
 class RetrofitPokedexNetworkDataSource @Inject constructor(
-    private val pokemonApi: PokemonApi,
+    private val pokemonApi: PokemonApi
 ) : PokedexNetworkDataSource {
     override suspend fun getPokemons(): List<NetworkPokemon> = coroutineScope {
         val ids = pokemonApi.getPokemons().results.orEmpty().mapNotNull { it.id }
--- app/src/main/java/dev/hinaka/pokedex/feature/pokemon/PokemonScreen.kt
+++ app/src/main/java/dev/hinaka/pokedex/feature/pokemon/PokemonScreen.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2022 Hinaka (Trung Nguyễn Minh Trần)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.hinaka.pokedex.feature.pokemon
 
 import androidx.compose.foundation.lazy.LazyColumn
@@ -14,13 +29,13 @@ import dev.hinaka.pokedex.domain.Pokemon
 @Composable
 fun PokemonRoute(
     modifier: Modifier = Modifier,
-    pokemonViewModel: PokemonViewModel = viewModel(),
+    pokemonViewModel: PokemonViewModel = viewModel()
 ) {
     val uiState by pokemonViewModel.uiState.collectAsState()
 
     PokemonScreen(
         pokemons = uiState.pokemons,
-        modifier = modifier,
+        modifier = modifier
     )
 }
 
@@ -31,7 +46,7 @@ fun PokemonScreen(
 ) {
     LazyColumn(
         modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         items(pokemons) { pokemon ->
             Text(text = pokemon.name)
