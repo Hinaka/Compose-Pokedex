af9396b4f038e6d4e13370249c8d88b19feaca69
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-09-09 18:32:20 +0700

    add modal navigation to main screen

--- app/src/main/java/dev/hinaka/pokedex/ui/PokedexApp.kt
+++ app/src/main/java/dev/hinaka/pokedex/ui/PokedexApp.kt
@@ -15,29 +15,78 @@
  */
 package dev.hinaka.pokedex.ui
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DrawerValue.Closed
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dev.hinaka.pokedex.core.designsystem.theme.PokedexTheme
 import dev.hinaka.pokedex.feature.pokemon.PokemonRoute
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PokedexApp() {
     PokedexTheme {
         val navController = rememberNavController()
-        // A surface container using the 'background' color from the theme
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
+        val drawerState = rememberDrawerState(initialValue = Closed)
+        val scope = rememberCoroutineScope()
+        val items = listOf(
+            "Pokedex",
+            "Move Dex",
+            "Ability Dex",
+            "Item Dex",
+            "Location Dex",
+            "Type Dex",
+            "Nature Dex",
+        )
+        var selectedItem by remember { mutableStateOf(items.first()) }
+        ModalNavigationDrawer(
+            drawerState = drawerState,
+            drawerContent = {
+                items.forEach { item ->
+                    NavigationDrawerItem(
+                        label = { Text(text = item) },
+                        selected = item == selectedItem,
+                        onClick = {
+                            scope.launch { drawerState.close() }
+                            selectedItem = item
+                        }
+                    )
+                }
+            }
         ) {
-            NavHost(navController = navController, startDestination = "pokemon") {
-                composable("pokemon") {
-                    PokemonRoute()
+            Scaffold(
+                topBar = {
+                    SmallTopAppBar(
+                        title = {
+                            Text(text = "Pokedex")
+                        }
+                    )
+                },
+            ) { contentPadding ->
+                NavHost(
+                    navController = navController,
+                    startDestination = "pokemon",
+                    modifier = Modifier.padding(contentPadding)
+                ) {
+                    composable("pokemon") {
+                        PokemonRoute()
+                    }
                 }
             }
         }
