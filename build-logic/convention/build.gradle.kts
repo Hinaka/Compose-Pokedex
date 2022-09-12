c926fdd07f944bdac39044d35d6ed9a22a165dd6
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-09-12 17:28:37 +0700

    common plugin for compose library

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -1,29 +1,33 @@
 plugins {
-  `kotlin-dsl`
+    `kotlin-dsl`
 }
 
 group = "dev.hinaka.pokedex.buildlogic"
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_1_8
-  targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 dependencies {
-  compileOnly(libs.android.gradleplugin)
-  compileOnly(libs.kotlin.gradleplugin)
-  compileOnly(libs.spotless.gradleplugin)
+    compileOnly(libs.android.gradleplugin)
+    compileOnly(libs.kotlin.gradleplugin)
+    compileOnly(libs.spotless.gradleplugin)
 }
 
 gradlePlugin {
-  plugins {
-    register("androidLibrary") {
-      id = "pokedex.android.library"
-      implementationClass = "AndroidLibraryConventionPlugin"
+    plugins {
+        register("androidLibrary") {
+            id = "pokedex.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+        register("androidLibraryCompose") {
+            id = "pokedex.android.library.compose"
+            implementationClass = "AndroidLibraryComposeConventionPlugin"
+        }
+        register("spotless") {
+            id = "pokedex.spotless"
+            implementationClass = "SpotlessConventionPlugin"
+        }
     }
-    register("spotless") {
-      id = "pokedex.spotless"
-      implementationClass = "SpotlessConventionPlugin"
-    }
-  }
 }
