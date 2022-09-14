143756845db285274b98deca72703636ffc12cee
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-09-14 17:34:19 +0700

    update ui for ability

--- feature/ability/src/main/java/dev/hinaka/pokedex/feature/ability/AbilityRoute.kt
+++ feature/ability/src/main/java/dev/hinaka/pokedex/feature/ability/AbilityRoute.kt
@@ -16,14 +16,21 @@
 package dev.hinaka.pokedex.feature.ability
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -72,5 +79,21 @@ fun Ability(
     ability: Ability,
     modifier: Modifier = Modifier,
 ) {
-    Text(text = ability.name)
+    Card(
+        modifier = modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.inverseSurface
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(vertical = 8.dp, horizontal = 16.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(text = ability.name, style = MaterialTheme.typography.titleMedium)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = ability.effect, style = MaterialTheme.typography.bodyMedium)
+        }
+    }
 }
