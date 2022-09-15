c9dd96f2a604f5a0fb01a39c6d4ebef789d52914
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-09-15 20:23:19 +0700

    feat: splash screen (#16)

--- app/src/main/java/dev/hinaka/pokedex/MainActivity.kt
+++ app/src/main/java/dev/hinaka/pokedex/MainActivity.kt
@@ -18,12 +18,14 @@ package dev.hinaka.pokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import dev.hinaka.pokedex.ui.PokedexApp
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             PokedexApp()
