742805d1006adbd05a9dbe54a5fddcd14dbc270e
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-10-21 18:24:09 +0700

    base ui for ExtraInfoSections

--- feature/pokemon/src/main/java/dev/hinaka/pokedex/feature/pokemon/ui/details/ExtraInfoSections.kt
+++ feature/pokemon/src/main/java/dev/hinaka/pokedex/feature/pokemon/ui/details/ExtraInfoSections.kt
@@ -1,22 +1,83 @@
 package dev.hinaka.pokedex.feature.pokemon.ui.details
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import dev.hinaka.pokedex.core.designsystem.component.PkdxCard
+import dev.hinaka.pokedex.core.designsystem.component.Space
+import dev.hinaka.pokedex.core.designsystem.theme.PokedexTheme
+import dev.hinaka.pokedex.core.ui.type.getTypeContainerColors
+import dev.hinaka.pokedex.core.ui.utils.preview.PokedexPreviews
+import dev.hinaka.pokedex.core.ui.utils.preview.PokemonPreviewParameterProvider
+import dev.hinaka.pokedex.domain.pokemon.Pokemon
 
 @Composable
 fun ExtraInfoSections(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier,
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = modifier.verticalScroll(rememberScrollState()),
     ) {
-        Text(text = "More")
+        Text(
+            text = "Damage taken",
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.titleMedium
+        )
+        Space(dp = 8.dp)
+        PkdxCard(modifier = Modifier.fillMaxWidth()) {
+
+        }
+        Space(dp = 16.dp)
+        Text(
+            text = "Sprites",
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.titleMedium
+        )
+        Space(dp = 8.dp)
+        PkdxCard(modifier = Modifier.fillMaxWidth()) {
+            
+        }
+        Space(dp = 16.dp)
+        Text(
+            text = "Training",
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.titleMedium
+        )
+        Space(dp = 8.dp)
+        PkdxCard(modifier = Modifier.fillMaxWidth()) {
+            
+        }
+        Space(dp = 16.dp)
+        Text(
+            text = "Breeding",
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.titleMedium
+        )
+        PkdxCard(modifier = Modifier.fillMaxWidth()) {
+            
+        }
+    }
+}
+
+@PokedexPreviews
+@Composable
+private fun ExtraInfoSectionsPreviews(
+    @PreviewParameter(PokemonPreviewParameterProvider::class, limit = 1) pokemon: Pokemon
+) {
+    PokedexTheme {
+        val (containerColor, contentColor) = pokemon.types.getTypeContainerColors()
+
+        Surface(color = containerColor, contentColor = contentColor) {
+            ExtraInfoSections()
+        }
     }
 }
