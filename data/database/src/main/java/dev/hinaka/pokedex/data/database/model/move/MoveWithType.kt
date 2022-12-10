52b561fd9e713fa5cbc34ee803d338c7c30226e4
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-12-10 16:12:21 +0700

    create MoveBuilder

--- data/database/src/main/java/dev/hinaka/pokedex/data/database/model/move/MoveWithType.kt
+++ data/database/src/main/java/dev/hinaka/pokedex/data/database/model/move/MoveWithType.kt
@@ -3,9 +3,7 @@ package dev.hinaka.pokedex.data.database.model.move
 import androidx.room.Embedded
 import androidx.room.Relation
 import dev.hinaka.pokedex.data.database.model.type.TypeEntity
-import dev.hinaka.pokedex.data.database.model.type.toDomain
-import dev.hinaka.pokedex.domain.Id
-import dev.hinaka.pokedex.domain.move.Move
+import dev.hinaka.pokedex.domain.move.move
 
 data class MoveWithType(
     @Embedded val moveEntity: MoveEntity,
@@ -16,13 +14,15 @@ data class MoveWithType(
     val typeEntity: TypeEntity
 )
 
-fun MoveWithType.toDomain() = Move(
-    id = Id(moveEntity.id),
-    name = moveEntity.name.orEmpty(),
-    type = typeEntity.toDomain()!!,
-    damageClass = moveEntity.damageClass!!,
-    power = moveEntity.power ?: 0,
-    acc = moveEntity.acc ?: 0,
-    pp = moveEntity.pp ?: 0
-)
+fun MoveWithType.toDomain() = move(moveEntity.id) {
+    name = moveEntity.name
+    type(typeEntity.id) {
+        name = typeEntity.name
+        identifier = typeEntity.identifier
+    }
+    damageClass = moveEntity.damageClass
+    power = moveEntity.power
+    acc = moveEntity.acc
+    pp = moveEntity.pp
+}
 
